b50d82baf84070dae24bf970d4bbb490dfb5bc2a
Daniel Salcedo <daniel@backapp.agency>
2024-02-14 18:19:37 -0600

    Feature: add opt and send to

--- README.md
+++ README.md
@@ -5,33 +5,23 @@ Install
 composer require wappico/wappi-php-client
 ```
 
-Usage:
+You may only contact people on WhatsApp if: (a) they have given you their mobile phone number; and (b) you have received opt-in permission from the recipient confirming that they wish to receive subsequent messages from you on WhatsApp
+* The opt-in must (a) clearly state that the person is opting in to receive messages from you over WhatsApp and (b) clearly state your business' name.
+* You are solely responsible for determining the method of opt-in, that you have obtained opt-in in a manner that complies with laws applicable to your communications, and that you have otherwise provided notices and obtained permissions that are required under applicable law.
+* To help ensure a strong user experience, review the best practices suggested below. https://business.whatsapp.com/policy#best_practices_for_optin
 
 ```php
-use Wappi\Client\Wappi;
+$whatsapp = new \Wappi\Client\Strategies\Whatsapp(apiKey: '')
 
-
-$wappi = new Wappi(
-    new \Wappi\Client\Strategies\Whatsapp(apiKey: '')
-);
-
-$wappi->add(
-    \Wappi\Client\Message\Message::create(["text" => "¡Hi there!"])
-);
-
-$wappi->addMany(
-    \Wappi\Client\Message\Message::create(["text" => "¡Hi there!"]),
-    \Wappi\Client\Message\Image::create(["url" => "https://wappi.co/image.png", "text" => "¡Hi there!"]),
-);
-
-$wappi->send();
+$whatsapp->phoneNumber("521782003377")->markAsOptIn();
 ```
 
-Or you can use the strategy:
+Send messages to one phone number
 
 ```php
-
 $whatsapp = new \Wappi\Client\Strategies\Whatsapp(apiKey: '')
+
+$whatsapp->phoneNumber("521782003377");
 
 $whatsapp->request([
     \Wappi\Client\Message\Message::create(["text" => "¡Hi there!"]),
--- src/Strategies/Whatsapp.php
+++ src/Strategies/Whatsapp.php
@@ -7,6 +7,10 @@ use Wappi\Client\Interfaces\Strategy;
 
 class Whatsapp implements Strategy
 {
+    private $phone;
+
+    const API_OPT_URL = 'https://wappi.co/api/v1/whatsapp/opt-in';
+
     /**
      * @param string $apiKey
      * @param string $apiHost
@@ -28,6 +32,33 @@ class Whatsapp implements Strategy
     }
 
     /**
+     * @param $phoneNumber
+     * @return $this
+     */
+    public function phoneNumber($phoneNumber)
+    {
+        $this->phone = $phoneNumber;
+        return $this;
+    }
+
+    /**
+     * @return \GuzzleHttp\Promise\PromiseInterface|\Illuminate\Http\Client\Response
+     */
+    public function markAsOptIn(): \GuzzleHttp\Promise\PromiseInterface|\Illuminate\Http\Client\Response
+    {
+        return Http::withToken(
+            $this->apiKey
+        )->timeout(
+            10
+        )->withHeaders([
+            'User-Agent' => 'Wappi/0.0.1',
+        ])->post(self::API_OPT_URL, [
+            'phoneNumber' => $this->phone,
+            'signature' => $this->signature($this->phone)
+        ]);
+    }
+
+    /**
      * @param array $data
      * @return \GuzzleHttp\Promise\PromiseInterface|\Illuminate\Http\Client\Response
      */
@@ -40,6 +71,7 @@ class Whatsapp implements Strategy
         )->withHeaders([
             'User-Agent' => 'Wappi/0.0.1',
         ])->post($this->apiHost, [
+            'phoneNumber' => $this->phone,
             'messages' => $data,
             'signature' => $this->signature($data)
         ]);
@@ -58,6 +90,7 @@ class Whatsapp implements Strategy
         )->withHeaders([
             'User-Agent' => 'Wappi/0.0.1',
         ])->async()->post($this->apiHost, [
+            'phoneNumber' => $this->phone,
             'messages' => $data,
             'signature' => $this->signature($data)
         ]);
